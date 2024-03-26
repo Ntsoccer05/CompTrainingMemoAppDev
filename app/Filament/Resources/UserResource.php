78160bf36418ad0a65a85ab566da468c981d17dd
EC2 Default User <ec2-user@webserver.localdomain>
2024-03-27 05:14:12 +0900

    fix filament setting

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -43,8 +43,8 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('email')->searchable(),
                 Tables\Columns\TextColumn::make('email_verified_at')
                     ->dateTime('Y年Md日 h:m:s'),
                 Tables\Columns\TextColumn::make('created_at')
